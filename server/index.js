499a90ea1ac83ca076930aefbf4431d82f6a8feb
PriyalMangal50 <145255577+PriyalMangal50@users.noreply.github.com>
2025-01-23 22:02:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,9 @@ const connection = require("./database/db.js");
 const product_list = require("./product_list.js");
 const PORT = 3001;
 
-app.use(cors());
+app.use(cors({
+    origin: "https://laundry-gmj8.vercel.app"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
